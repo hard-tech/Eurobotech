f103acc622bf92eddccc96bbb0a06b2eea024bc8
new-tech <77115651+hard-tech@users.noreply.github.com>
2022-04-06 23:31:36 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,26 @@
-//
+document.querySelector("footer").innerHTML = `
+<div class="trans text-center">
+<h2 class="trans-txt"><b>Plus de contenu prochainement !</b></h2>
+</div>
+<div class="footer-info">
+<iframe class="map center px-4 py-4" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d733.5773620461221!2d2.67986141088268!3d49.0643558431543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e623163b50908b%3A0x98568290487beaaf!2sLyc%C3%A9e%20Charlotte%20Delbo!5e0!3m2!1sfr!2sfr!4v1640451484761!5m2!1sfr!2sfr" allowfullscreen="" loading="lazy"></iframe>
+<div class="footer-info-txt">
+    <div class="textwidget px-4 py-4">
+    <p>
+        Email: ce.0772751X@ac-creteil.fr<br>
+        Téléphone:  01.64.76.06.80<br>
+        Adresse: 2 avenue Robert Badinter, 77230 Dammartin en Goële FRANCE<br><br>
+        Notre établissement accueille le public aux horaires suivantes :<br>
+        8hoo à 12h30 - 13h30 à 17h30 - Lundi, Mardi, Jeudi, Vendredi <br>
+        et le Mercredi de 8hoo à 12hoo <br><br>
+        <div class="text-center"><b>Le site web est en cours de validation par le lycée.</b></div>
+    </p>
+    </div>
+</div>
+</div>
+`;
+document.querySelector(".footer").innerHTML = `
+<div class="footer-copyr text-center py-2">© 2022 Copyright by New-webTech (Ahmad JABER) :
+site v4.1
+</div>  
+`;
